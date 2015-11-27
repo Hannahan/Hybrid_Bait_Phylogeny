2155852f988423b88cc536fc212b395e1c85f4cc
CKidner <c.kidner@rbge.ac.uk>
2015-11-27 09:14:06 +0000

    kicking raw_to_bowtie.sh

--- raw_to_bowtie.sh
+++ raw_to_bowtie.sh
@@ -27,9 +27,9 @@ cutadapt -a AGATCGGAAGAGC -o f_unpaired.fq.gz forward_unpaired.fq.gz 2>> cut_out
 cutadapt -a AGATCGGAAGAGC -o r_unpaired.fq.gz reverse_unpaired.fq.gz 2>> cut_out
 
 #remove unpaired
-cat f_paired.fq.gz r_paired.fq.gz | grep -B1 "^$" | grep "^@" | cut -f1 -d " " - > All.empties
+cat forward_paired.fq.gz reverse_paired.fq.gz | grep -B1 "^$" | grep "^@" | cut -f1 -d " " - > All.empties
 
-cat r_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 2.fastq.test.gz; mv 2.fastq.test.gz r_paired.fq.gz
-cat f_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 1.fastq.test.gz; mv 1.fastq.test.gz f_paired.fq.gz
+cat reverse_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 2.fastq.test.gz; mv 2.fastq.test.gz reverse_paired.fq.gz
+cat forward_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 1.fastq.test.gz; mv 1.fastq.test.gz forward_paired.fq.gz
 
 bowtie2 --local  -x ~/bowtie_index/All_baits -1 forward_paired.fq.gz  -2 reverse_paired.fq.gz  -U forward_unpaired.fq.gz,reverse_unpaired.fq.gz  -S output.sam 2> $bowtie
