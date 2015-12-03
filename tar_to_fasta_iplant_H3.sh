deca55a6acafa57ae72b9b8b2def725e10b2cb09
CKidner <c.kidner@rbge.ac.uk>
2015-12-03 21:37:34 +0000

    tar to fdasts H3

--- tar_to_fasta_iplant_H3.sh
+++ tar_to_fasta_iplant_H3.sh
@@ -21,35 +21,7 @@ vcf=${acc}.vcf
 
 echo "You're working on accession $1"
 
-
-#get the trimmed tar from process folder and tidying up the old mess
-cp ~/Documents/Process/Inga_Baits/$tar ./
-tar -zxvf $tar
-
-rm forward*
-rm reverse*
-
-mv f_unpaired.fq.gz f_unpaired.fq
-mv r_unpaired.fq.gz r_unpaired.fq
-
-# leaving the trimming here in case I need to change this to work on the raw reads in future
-
-#Trimmomatic
-#java -jar ~/Documents/Trimmomatic-0.33/trimmomatic-0.33.jar PE -phred33 $F $R forward_paired.fq.gz forward_unpaired.fq.gz reverse_paired.fq.gz reverse_unpaired.fq.gz ILLUMINACLIP:../Trimmomatic-0-0.33/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-
-#cutadapt
-#cutadapt -a AGATCGGAAGAGC -o f_paired.fq.gz forward_paired.fq.gz  2>> cut_out
-#cutadapt -a AGATCGGAAGAGC -o r_paired.fq.gz reverse_paired.fq.gz 2>> cut_out
-#cutadapt -a AGATCGGAAGAGC -o f_unpaired.fq.gz forward_unpaired.fq.gz 2>> cut_out
-#cutadapt -a AGATCGGAAGAGC -o r_unpaired.fq.gz reverse_unpaired.fq.gz 2>> cut_out
-
-#remove unpaired
-#cat f_paired.fq.gz r_paired.fq.gz | grep -B1 "^$" | grep "^@" | cut -f1 -d " " - > All.empties
-
-#cat r_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 2.fastq.test.gz; mv 2.fastq.test.gz r_paired.fq.gz
-#cat f_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 1.fastq.test.gz; mv 1.fastq.test.gz f_paired.fq.gz
-
-bowtie2 --local  --score-min G,130,8 -x ~/bowtie_index/All_loci -1 $F  -2 $R  -U $Fu,$Ru  -S output.sam 2> $bowtie
+bowtie2 --local  --score-min G,130,8 -x ~/bowtie_index/All_loci -1 $F -2 $R -U $Fu,$Ru -S output.sam 2> $bowtie
 
 samtools view -bS output.sam | samtools sort - bam_sorted
 samtools index bam_sorted.bam
