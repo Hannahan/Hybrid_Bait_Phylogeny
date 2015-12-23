51ce12a4fd95407764470f082e250d4d6ea637d7
Catherine Kidner <ckidner@hfmac03.rbge.org.uk>
2015-12-23 17:31:27 +0000

    vcf to polymorph to run over night

--- vcf_to_polymorph.sh
+++ vcf_to_polymorph.sh
@@ -14,7 +14,7 @@ while read f ; do grep -m 1000 "$f" $vcf >> $1K ; done < For_pop_gen
 #Per locus per accession count the polymorphisms from the ref
 #loop through loci and accessions with:
 
-while read f ; do grep "$f" $1K | grep "GT:PL:GQ" | grep  :[3-9][0-9]$ > $poly
+while read f ; do grep "$f" $1K | grep "GT:PL:GQ" | grep  :[3-9][0-9]$ > $poly ; done < For_pop_gene
 
 #Per accession pair pull out the un-shared polymorphisms and count these per locus
 #join -v 1 set1_poly set2_poly > dif
@@ -23,5 +23,5 @@ while read f ; do grep "$f" $1K | grep "GT:PL:GQ" | grep  :[3-9][0-9]$ > $poly
 #Per accession pair count how many of the un-shared polymorphisms to ref are heterozygous
 #The counts for the shared polymorphisms to ref and the shared heterozygous polymorphism to ref will be calculated
 
-
+exit 0
 
