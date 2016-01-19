59cfe647ef74a8a139cae082b0db39a54a676ae5
Catherine Kidner <ckidner@hfmac03.rbge.org.uk>
2016-01-19 12:00:23 +0000

    umb_pop_H6and7 re un

---
+++ umb_pop_130.sh
@@ -0,0 +1,76 @@
+#! /bin/bash -x
+# to go from raw seq to consensus on iplant
+# Assumes files are in form *_1.sanfastq.gz and are in the folder ~/Documents/Process/Inga_baits
+# Assumes you're working in the folder Process - the attached Volume
+
+# Catherine Kidner 23 Oct 2015
+
+
+echo "Hello world"
+
+acc=$1
+
+F=~/Documents/Process/raw_reads/${acc}_1.fastq.gz
+R=~/Documents/Process/raw_reads/${acc}_2.fastq.gz
+
+output=~/Documents/iROD/done_consensuses/${acc}_consensus.fna
+rc=~/Documents/iROD/done_consensuses/${acc}_rc.txt
+bowtie=~/Documents/iROD/done_consensuses/${acc}_bowtie_output
+
+echo "You're working on accession $1"
+
+# leaving the from-trimmed here in case I need to change this in future
+
+#get the trimmed tar from iROD folder and tidying up the old mess
+#cp ~/Documents/iROD/Inga_Baits/$tar ./
+#tar -zxvf $tar
+
+#rm forward*
+#rm reverse*
+
+#mv f_unpaired.fq.gz f_unpaired.fq
+#mv r_unpaired.fq.gz r_unpaired.fq
+
+#Trimmomatic
+java -jar ~/Documents/Trimmomatic-0.33/trimmomatic-0.33.jar PE -phred33 $F $R forward_paired.fq.gz forward_unpaired.fq.gz reverse_paired.fq.gz reverse_unpaired.fq.gz ILLUMINACLIP:../Trimmomatic-0-0.33/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+#cutadapt
+#cutadapt -a AGATCGGAAGAGC -o f_paired.fq.gz forward_paired.fq.gz  2>> cut_out
+#cutadapt -a AGATCGGAAGAGC -o r_paired.fq.gz reverse_paired.fq.gz 2>> cut_out
+#cutadapt -a AGATCGGAAGAGC -o f_unpaired.fq.gz forward_unpaired.fq.gz 2>> cut_out
+#cutadapt -a AGATCGGAAGAGC -o r_unpaired.fq.gz reverse_unpaired.fq.gz 2>> cut_out
+
+#remove unpaired
+#cat f_paired.fq.gz r_paired.fq.gz | grep -B1 "^$" | grep "^@" | cut -f1 -d " " - > All.empties
+
+#cat r_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 2.fastq.test.gz; mv 2.fastq.test.gz r_paired.fq.gz
+#cat f_paired.fq.gz | paste - - - - | grep -F -v -w -f All.empties - | tr "\t" "\n" | gzip > 1.fastq.test.gz; mv 1.fastq.test.gz f_paired.fq.gz
+
+bowtie2 --local  --score-min G,130,8 -x ~/bowtie_index/All_loci -1 forward_paired.fq.gz  -2 reverse_paired.fq.gz  -U forward_unpaired.fq.gz,reverse_unpaired.fq.gz  -S output.sam 2> $bowtie
+
+samtools view -bS output.sam | samtools sort - bam_sorted
+samtools index bam_sorted.bam
+samtools mpileup -E -uf ~/bowtie_index/All_loci.fna bam_sorted.bam > output.pileup
+bcftools view -cg output.pileup > output.vcf
+
+#get read_counts - reads 125bp long
+
+samtools idxstats bam_sorted.bam |grep -v "^\*" | awk '{ depth=125*$3/$2} {print $1, depth}' | sort > $rc
+
+rm *.sam
+rm *.pileup
+rm *.bam
+rm *.bai
+rm *.fq
+rm *.gz
+
+grep -v "INDEL" output.vcf | awk '{if ($6 >= 36) print $0}' > clean.vcf
+
+perl vcfutils_fasta.pl vcf2fq clean.vcf > $output
+
+#sed '/^[^>]/s/[^ATGCactg]/N/g' output.fna > $output 
+
+#rm *.vcf
+
+exit 0
+
