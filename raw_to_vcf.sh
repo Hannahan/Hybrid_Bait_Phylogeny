df69a66deacc3c7dd93e65a17bea18cf72f02c29
Catherine Kidner <c.kidner@rbge.ac.uk>
2016-12-05 09:47:23 +0000

    just checking

--- raw_to_vcf.sh
+++ raw_to_vcf.sh
@@ -9,13 +9,11 @@
 echo "Hello world"
 
 acc=$1
-tar=${acc}.tar.gz
-F=${acc}_trimmed_1.fastq.gz
-R=${acc}_trimmed_2.fastq.gz
-Fu=${acc}_trimmed_1u.fastq
-Ru=${acc}_trimmed_2u.fastq
-mv f_unpaired.fq.gz f_unpaired.fq
-mv r_unpaired.fq.gz r_unpaired.fq
+source_1=~/Process/raw_reads/${acc}_1.fastq.gz
+source_2=~/Process/raw_reads/${acc}_1.fastq.gz
+F=${acc}_1.fastq.gz
+R=${acc}_2.fastq.gz
+
 output=~/Documents/iROD/vcf_for_lyndsey/${acc}.vcf
 bowtie=${acc}_bowtie_out
 
@@ -23,10 +21,13 @@ echo "You're working on accession $1"
 
 # leaving the from-trimmed here in case I need to change this in future
 
-#get the trimmed tar from raw_reads folder and tidying up the old mess
+#get the trimmed tar from Process (the attched Volume) and tidying up the old mess
+cp $source_1 ./
+cp $source_2 ./
 
-tar -zxf raw_reads/$tar -C ./
-bowtie2 --local  --score-min G,320,8 -x ~/bowtie_index/All_loci -1 $F -2 $R -U $Fu,$Fr -S output.sam 2>$bowtie
+java -jar ~/Trimmomatic-0.33/trimmomatic-0.33.jar PE -phred33 $F $R f_prd.fq.gz f_uprd.fq.gz r_prd.fq.gz r_uprd.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+bowtie2 --local  --score-min G,320,8 -x ~/bowtie_index/All_loci -1 f_prd.fq -2 r_prd.fq.gz -U f_uprd.fq.gz,r_uprd.fq.gz -S output.sam 2>$bowtie
 
 samtools view -bS output.sam | samtools sort - bam_sorted
 samtools index bam_sorted.bam
